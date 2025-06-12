f4bf0b4f6aa4645011b60ef0aecccc92b9954599
Maqsud G <maqsudmuhammadiyev65@gmail.com>
2025-06-12 13:41:13 +0500

    UniHeader barcha h1 lar uchun universal

--- src/components/shared/SaleProduct/SaleProductsList.tsx
+++ src/components/shared/SaleProduct/SaleProductsList.tsx
@@ -1,11 +1,13 @@
 import { styles } from '@/styles/styles.ts'
 import SaleProductItem from './SaleProductItem.tsx'
 import type { ISaleProductsList } from '@/types/index.ts'
+import UniHeader from '../UniHeader.tsx'
 
 export default function SaleProductsList({ title, data }: ISaleProductsList) {
       return (
             <div className={`${styles.container} `}>
-                  <h1>{title}</h1>
+                  <UniHeader text={title} />
+                  
                   <div className='grid grid-cols-4 gap-7'>
                         {data.map((item, index) => <SaleProductItem data={item} key={index} />)}
                   </div>
---
+++ src/components/shared/UniHeader.tsx
@@ -0,0 +1,11 @@
+
+interface UniHeaderProps {
+    text:string
+}
+
+export default function UniHeader({ text }: UniHeaderProps) {
+  return (
+    <h1 className="dark:text-white text-[#292929] text-[30px] font-semibold
+     leading-[100%] text-center my-[40px]">{text}</h1>
+  );
+}
