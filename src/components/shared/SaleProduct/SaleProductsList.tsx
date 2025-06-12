10403ebb296c5380f0e4dc818bb53fd05826d317
Maqsud G <maqsudmuhammadiyev65@gmail.com>
2025-06-12 14:29:22 +0500

    mini Update

--- src/components/shared/SaleProduct/SaleProductsList.tsx
+++ src/components/shared/SaleProduct/SaleProductsList.tsx
@@ -4,30 +4,28 @@ import {
       Carousel,
       CarouselContent,
       CarouselItem,
-      CarouselNext,
-      CarouselPrevious,
 } from "@/components/ui/carousel"
 import Autoplay from 'embla-carousel-autoplay'
 import type { ISaleProductsList } from '@/types/index.ts'
 import UniHeader from '../UniHeader.tsx'
+import { cn } from '@/lib/utils.ts'
 
 
 export default function SaleProductsList({ title, data }: ISaleProductsList) {
       return (
             <div className={`${styles.container} `}>
                   <UniHeader text={title} />
-                  <Carousel className="w-full max-w-xs" opts={{ loop: true }} plugins={[Autoplay({ delay: 3000 })]}>
+                  <Carousel className={`${cn(`w-full`)}`} opts={{ loop: true }} plugins={[Autoplay({ delay: 3000 })]}>
                         <CarouselContent>
                               {data.map((item, index) => (
-                                    <CarouselItem key={index}>
+                                    <CarouselItem key={index} className={`${cn(`w-full max-w-[271px]`)}`}>
                                           <div className="p-1">
                                                 <SaleProductItem data={item} key={index} />
                                           </div>
                                     </CarouselItem>
                               ))}
                         </CarouselContent>
-                        <CarouselPrevious />
-                        <CarouselNext />
+
                   </Carousel>
             </div>
       )
